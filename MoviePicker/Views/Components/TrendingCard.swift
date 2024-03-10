50aedb5ac8cf47b78126050cf37f826448903d37
Damitha Raveendra <deamonsarea52@gmail.com>
2024-03-11 02:48:51 +0530

    * Fix issue with model * Srat building the trending card

---
+++ MoviePicker/Views/Components/TrendingCard.swift
@@ -0,0 +1,18 @@
+//
+//  TrendingCard.swift
+//  MoviePicker
+//
+//  Created by Damitha Raveendra on 2024-03-11.
+//
+
+import SwiftUI
+
+struct TrendingCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrendingCard()
+}
